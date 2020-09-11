4c92ebd3ed28e4cc119ed233e7dde98c8aa25701
atom-cmd <65195355+atom-cmd@users.noreply.github.com>
2020-09-11 16:15:05 -0400

    Delete Logger.ts

--- src/Logger.ts
+++
@@ -1,9 +0,0 @@
-import logger from 'pino';
-
-let options: any = { level: 'trace' };
-if (process.env.NODE_ENV !== 'production')
-    options = { level: 'trace', prettyPrint: true };
-
-const log = logger(options);
-
-export default log;
